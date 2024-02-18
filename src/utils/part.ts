33b676091ca654013665934f5f5a05fd61552a07
Ampoi <tange.toshihiro@outlook.jp>
2024-02-18 16:31:44 +0900

    [wip]いい感じにポートを使用するようにした

--- src/utils/part.ts
+++ src/utils/part.ts
@@ -5,16 +5,41 @@ export type PartName = typeof partNames[number]
 
 const size = 40
 
+class Port {
+    powerIsOn = false
+    battery: { now: number, max: number }
+    
+    constructor( part: Part ){
+        if( !part.energy ) throw new Error("ポートを用いているクラスは電力を使用していません")
+        this.battery = part.energy.battery
+    }
+
+    public chargeBattery(energy: number){
+        if( !this.battery ) throw new Error("バッテリーが存在しないので充電できません！")
+        const newBattery = this.battery.now + energy
+        this.battery.now = newBattery > this.battery.max ? this.battery.max : newBattery
+        this.powerIsOn = true
+    }
+
+    public useBattery(energy: number){
+        if( !this.battery ) throw new Error("バッテリーが存在しないので使用できません！")
+        const newBattery = this.battery.now - energy
+        this.battery.now = newBattery < 0 ? 0 : newBattery
+    }
+}
+
 export abstract class Part {
     abstract readonly name: PartName
     abstract readonly health: number
-    abstract readonly battery: {
-        now: number
-        max: number
+    abstract readonly energy: {
+        readonly battery: {
+            now: number
+            max: number
+        }
+        ports: Port[]
     } | undefined
-    abstract readonly port: undefined | ["charge"] | ["charge", "triangle"] | ["charge", "triangle", "square"]
+    
     abstract customDrawFunc(p: p5): void
-
     abstract readonly action?: () => void
 
     readonly position: Vector
@@ -27,18 +52,6 @@ export abstract class Part {
         this.connectedToTileID = connectedToTileID
     }
 
-    public chargeBattery(energy: number){
-        if( !this.battery ) throw new Error("バッテリーが存在しないので充電できません！")
-        const newBattery = this.battery.now + energy
-        this.battery.now = newBattery > this.battery.max ? this.battery.max : newBattery
-    }
-
-    public useBattery(energy: number){
-        if( !this.battery ) throw new Error("バッテリーが存在しないので使用できません！")
-        const newBattery = this.battery.now - energy
-        this.battery.now = newBattery < 0 ? 0 : newBattery
-    }
-
     draw(p: p5, position: Vector, angle: number){
         p.translate(position.x*size, position.y*size)
         p.rotate(angle)
@@ -49,12 +62,12 @@ export abstract class Part {
 
         this.customDrawFunc(p)
 
-        if( this.battery ){
+        if( this.energy ){
             p.noStroke()
             p.fill(150)
             p.circle(0, 0, size * 0.85)
             p.fill("#0ac729")
-            p.arc(0, 0, size*0.85, size*0.85, -Math.PI/2, Math.PI * (this.battery.now / this.battery.max * 2 - 1/2));
+            p.arc(0, 0, size*0.85, size*0.85, -Math.PI/2, Math.PI * (this.energy.battery.now / this.energy.battery.max * 2 - 1/2));
             p.fill(255)
             p.circle(0, 0, size * 0.7)
         }
@@ -72,7 +85,7 @@ export abstract class Part {
 class Block extends Part {
     readonly name = "Block"
     readonly health = 100
-    readonly battery = undefined
+    readonly energy = undefined
 
     customDrawFunc(p: p5): void {
         p.square(
@@ -86,9 +99,12 @@ class Block extends Part {
 class Motor extends Part {
     readonly name = "Motor"
     readonly health = 100
-    readonly battery = {
-        now: 0,
-        max: 100
+    readonly energy: Exclude<Part["energy"], undefined> = {
+        battery: {
+            now: 0,
+            max: 100
+        },
+        ports: [ new Port(this) ]
     }
 
     customDrawFunc(p: p5): void {
@@ -103,9 +119,12 @@ class Motor extends Part {
 class Engine extends Part {
     readonly name = "Engine"
     readonly health = 100
-    readonly battery = {
-        now: 0,
-        max: 100
+    readonly energy: Exclude<Part["energy"], undefined> = {
+        battery: {
+            now: 0,
+            max: 100
+        },
+        ports: [ new Port(this) ]
     }
 
     customDrawFunc(p: p5): void {
@@ -126,9 +145,9 @@ class Engine extends Part {
     private readonly useBatteryAmount = 2
 
     readonly action = () => {
-        if( this.battery.now > 0  ){
+        if( this.energy.battery.now > 0  ){
             this.isOn = true
-            this.useBattery(this.useBatteryAmount)
+            this.energy.ports[0].useBattery(this.useBatteryAmount)
             //TODO: ロケットの加速をする
         }else{
             this.isOn = false
@@ -138,9 +157,12 @@ class Engine extends Part {
 class Battery extends Part {
     readonly name = "Battery"
     readonly health = 100
-    readonly battery = {
-        now: 2000,
-        max: 2000
+    readonly energy: Exclude<Part["energy"], undefined> = {
+        battery: {
+            now: 2000,
+            max: 2000
+        },
+        ports: [ new Port(this) ]
     }
     customDrawFunc(p: p5): void {
         p.fill(100)
@@ -156,11 +178,14 @@ class Battery extends Part {
 class Button extends Part {
     readonly name = "Button"
     readonly health = 100
-    readonly battery = {
-        now: 0,
-        max: 100
+    readonly energy: Exclude<Part["energy"], undefined> = {
+        battery: {
+            now: 0,
+            max: 100
+        },
+        ports: [ new Port(this) ]
     }
-    private isClicked = false
+
     customDrawFunc(p: p5): void {
         p.square(
             0, 0, size,
@@ -169,12 +194,7 @@ class Button extends Part {
         p.fill(200, 0, 0)
         p.rect(0, -size*0.6, size*0.6, size*0.2)
     }
-    public useBattery(energy: number): void {
-        if( this.isClicked ){
-            const newBattery = this.battery.now - energy
-            this.battery.now = newBattery < 0 ? 0 : newBattery
-        }
-    }
+
     action = () => {
     }
 }
